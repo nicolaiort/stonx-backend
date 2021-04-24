309a64a26f32b7506723873d3e9df3b584990a90
Nicolai Ort <info@nicolai-ort.com>
2021-04-24 20:12:06 +0200

    Now with wokring index assets

--- src/controllers/BitpandaController.ts
+++ src/controllers/BitpandaController.ts
@@ -6,7 +6,7 @@ import { Wallet } from "src/models/Wallet";
 
 @Controller("/bitpanda")
 export class BitpandaController {
-    @Get("assets/crypto")
+    @Get("/assets/crypto")
     @Description("Returns your bitpanda wallets by coin with balance and fiat equivalent.")
     @Returns(200, Wallet)
     async getCryptoAssets(@QueryParams("withEmpty") withEmpty: boolean = false): Promise<Wallet[]> {
@@ -22,6 +22,33 @@ export class BitpandaController {
         for (let wallet of wallets) {
             if (!withEmpty && parseFloat(wallet.attributes.balance) != 0) {
                 returnWallets.push(new Wallet(wallet.attributes.cryptocoin_symbol, parseFloat(wallet.attributes.balance), parseFloat(prices[wallet.attributes.cryptocoin_symbol][config["CURRENCY"]])));
+            }
+            else{
+                returnWallets.push(new Wallet(wallet.attributes.cryptocoin_symbol, parseFloat(wallet.attributes.balance), parseFloat(prices[wallet.attributes.cryptocoin_symbol][config["CURRENCY"]])));
+            }
+        }
+
+        return returnWallets;
+    }
+
+    @Get("/assets/index")
+    @Description("Returns your bitpanda crypto index wallets with balance.")
+    @Returns(200, Wallet)
+    async getIndexAssets(@QueryParams("withEmpty") withEmpty: boolean = false): Promise<Wallet[]> {
+        let indices = (await axios.get('https://api.bitpanda.com/v1/asset-wallets', {
+            headers: {
+                'X-API-KEY': config["BITPANDA_API_KEY"]
+            }
+        })).data.data.attributes.index.index.attributes.wallets;
+
+        let returnWallets: Wallet[] = new Array<Wallet>();
+
+        for (let index of indices) {
+            if (!withEmpty && parseFloat(index.attributes.balance) != 0) {
+                returnWallets.push(new Wallet(index.attributes.cryptocoin_symbol, parseFloat(index.attributes.balance), 1));
+            }
+            else{
+                returnWallets.push(new Wallet(index.attributes.cryptocoin_symbol, parseFloat(index.attributes.balance), 1))
             }
         }
 
