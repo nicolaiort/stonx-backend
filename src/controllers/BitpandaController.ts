a25d01406276219438ed1258012641f649a86a97
Nicolai Ort <info@nicolai-ort.com>
2021-04-24 19:55:49 +0200

    Now delivering wallets with prives

--- src/controllers/BitpandaController.ts
+++ src/controllers/BitpandaController.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, QueryParams} from "@tsed/common";
+import { Controller, Get, QueryParams } from "@tsed/common";
 import { Description, Returns } from "@tsed/schema";
 import axios from "axios";
 import { config } from "src/config/env";
@@ -6,30 +6,25 @@ import { Wallet } from "src/models/Wallet";
 
 @Controller("/bitpanda")
 export class BitpandaController {
-  @Get("/ticker")
-  async findAll(): Promise<string> {
-    let ticker:Map<string, object> = await axios.get('https://api.bitpanda.com/v1/ticker');
-    return ticker.keys.toString();
-  }
+    @Get("/wallets")
+    @Description("Returns your bitpanda wallets by coin with balance.")
+    @Returns(200, Wallet)
+    async getWallets(@QueryParams("withEmpty") withEmpty: boolean = false): Promise<Wallet[]> {
+        let wallets = (await axios.get('https://api.bitpanda.com/v1/wallets', {
+            headers: {
+                'X-API-KEY': config["BITPANDA_API_KEY"]
+            }
+        })).data.data;
+        let prices = (await axios.get('https://api.bitpanda.com/v1/ticker')).data
 
-  @Get("/wallets")
-  @Description("Returns your bitpanda wallets by coin with balance.")
-  @Returns(200, Wallet)
-  async getWallets(@QueryParams("withEmpty") withEmpty: boolean = false): Promise<Wallet[]> {
-    let wallets = (await axios.get('https://api.bitpanda.com/v1/wallets', {
-        headers:{
-            'X-API-KEY': config["BITPANDA_API_KEY"]
+        let returnWallets: Wallet[] = new Array<Wallet>();
+
+        for (let wallet of wallets) {
+            if (!withEmpty && parseFloat(wallet.attributes.balance) != 0) {
+                returnWallets.push(new Wallet(wallet.attributes.cryptocoin_symbol, parseFloat(wallet.attributes.balance), parseFloat(prices[wallet.attributes.cryptocoin_symbol][config["CURRENCY"]])));
+            }
         }
-    })).data.data;
 
-    let returnWallets: Wallet[] = new Array<Wallet>();
-
-    for(let wallet of wallets){
-        if(!withEmpty && parseFloat(wallet.attributes.balance)!=0){
-            returnWallets.push(new Wallet(wallet.attributes.cryptocoin_symbol, parseFloat(wallet.attributes.balance)))
-        }
+        return returnWallets;
     }
-
-    return returnWallets;
-  }
 }
--- src/models/Wallet.ts
+++ src/models/Wallet.ts
@@ -1,16 +1,25 @@
 import {
+    Optional,
     Required
 } from "@tsed/schema";
 
 export class Wallet {
     @Required()
-    coin: string;
+    token: string;
 
     @Required()
     balance: number;
+    
+    @Optional()
+    fiat: number;
 
-    constructor(coin: string, balance:number){
-        this.coin=coin;
+    constructor(token: string, balance:number, price:number = 0){
+        this.token=token;
         this.balance=balance;
+        this.fiat=this.balance*price;
+    }
+
+    public set price(value:number){
+        this.fiat=this.balance*value;
     }
 }
