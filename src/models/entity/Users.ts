889b4a6d96c72ee2a083a1358ead69e31f78ca75
Nicolai Ort <info@nicolai-ort.com>
2021-04-26 16:44:06 +0200

    Changed structure to better fit the general theme

--- src/models/entity/Users.ts
+++ src/models/entity/Users.ts
@@ -2,7 +2,7 @@ import { Email } from "@tsed/schema";
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity()
-export class User {
+export class Users {
 
   @PrimaryGeneratedColumn("uuid")
   id: string;
