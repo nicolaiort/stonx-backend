37656217f59a1b994f72c27db6d69b9f63770e8c
Nicolai Ort <info@nicolai-ort.com>
2021-04-26 18:19:20 +0200

    Added basic Userservice

---
+++ src/services/users/UserService.ts
@@ -0,0 +1,20 @@
+import { UserCreation } from "src/models/UserCreation";
+import {Repository, EntityRepository} from "typeorm";
+import {User} from "../../models/entity/User";
+
+@EntityRepository(User)
+export class UserService extends Repository<User> {
+  async findById(id: string): Promise<User | undefined> {
+    return this.findOne({ id: id });
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.findOne({ email: email });
+  }
+
+  async createUser(new_user: UserCreation): Promise<User>{
+    let user = new User(new_user.email);
+    await user.setPassword(new_user.password);
+    return this.create(user);
+  }
+}
