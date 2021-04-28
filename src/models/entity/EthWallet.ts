fb28ac1ca415c502413c2bed3fd29af706d66a6e
Nicolai Ort <info@nicolai-ort.com>
2021-04-28 20:45:23 +0200

    Added basic ethwallet entity

---
+++ src/models/entity/EthWallet.ts
@@ -0,0 +1,34 @@
+import { Description, Email, Ignore, Optional, Required } from "@tsed/schema";
+import axios from "axios";
+import { config } from "src/config/env";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./User";
+
+@Entity()
+export class EthWallet {
+  @PrimaryGeneratedColumn("uuid")
+  @Description("Id assigned by the datbase.")
+  @Ignore()
+  id: string;
+
+  @Column({ nullable: true })
+  @Optional()
+  owner: User;
+
+  @Column({ nullable: false })
+  @Required()
+  token: string = "ETH";
+
+  @Column({ nullable: false })
+  address: string;
+
+  async balance():Promise<number>{
+    const res = await axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=${this.address}&tag=latest&apikey=${config["ETHERSCAN_APIKEY"]}`)
+    return parseFloat(res.data.result);
+  }
+
+  constructor(owner: User, address: string) {
+    this.address = address;
+    this.owner = owner;
+  }
+}
