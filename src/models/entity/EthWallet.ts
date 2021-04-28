677858d1bd6994245c920a71aed13469ab01a485
Nicolai Ort <info@nicolai-ort.com>
2021-04-28 20:58:33 +0200

    Adjusted to ethscans bs format

--- src/models/entity/EthWallet.ts
+++ src/models/entity/EthWallet.ts
@@ -23,7 +23,7 @@ export class EthWallet {
 
   async balance():Promise<number>{
     const res = await axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=${this.address}&tag=latest&apikey=${config["ETHERSCAN_APIKEY"]}`)
-    return parseFloat(res.data.result);
+    return (parseInt(res.data.result)*0.1);
   }
 
   constructor(owner: User, address: string) {
