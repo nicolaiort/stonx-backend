7fd9daa7adb6f4f92f96971951457d1faac794a2
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 18:13:53 +0200

    Now loading wallet prices from binance

--- src/controllers/WalletController.ts
+++ src/controllers/WalletController.ts
@@ -1,12 +1,12 @@
-import {BodyParams, Controller, Delete, Get, PathParams, Post, QueryParams, Req} from "@tsed/common";
-import {Authenticate, Authorize} from "@tsed/passport";
-import {Description, Returns, Security} from "@tsed/schema";
-import axios from "axios";
-import {config} from "src/config/env";
-import {User} from "src/models/entity/User";
-import {Wallet} from "src/models/Wallet";
-import {WalletCreation} from "src/models/WalletCreation";
-import {WalletService} from "src/services/users/WalletService";
+import { BodyParams, Controller, Delete, Get, PathParams, Post, Req } from "@tsed/common";
+import { Authenticate } from "@tsed/passport";
+import { Description, Returns, Security } from "@tsed/schema";
+import { config } from "src/config/env";
+import { User } from "src/models/entity/User";
+import { Wallet } from "src/models/Wallet";
+import { WalletCreation } from "src/models/WalletCreation";
+import { WalletService } from "src/services/users/WalletService";
+import { BinanceService } from "src/services/utils/BinanceService";
 
 @Controller("/wallets")
 export class WalletController {
@@ -21,12 +21,10 @@ export class WalletController {
       return [];
     }
 
-    let prices = (await axios.get("https://api.bitpanda.com/v1/ticker")).data;
-
     let returnWallets: Wallet[] = new Array<Wallet>();
 
     for (let wallet of wallets) {
-      returnWallets.push(new Wallet("ETH", await wallet.balance(), parseFloat(prices[wallet.token][config["CURRENCY"]]), wallet.id));
+      returnWallets.push(new Wallet("ETH", await wallet.balance(), (await BinanceService.getTradingPair(wallet.token, config["CURRENCY"])).price, wallet.id));
     }
 
     return returnWallets;
@@ -39,8 +37,7 @@ export class WalletController {
   @Returns(200, Wallet)
   async createWallets(@BodyParams() new_wallet: WalletCreation, @Req() req: Req): Promise<Wallet> {
     let wallet = await this.walletService.createWallet(req.user as User, new_wallet);
-    let prices = (await axios.get("https://api.bitpanda.com/v1/ticker")).data;
-    return new Wallet("ETH", await wallet.balance(), parseFloat(prices[wallet.token][config["CURRENCY"]]), wallet.id);
+    return new Wallet("ETH", await wallet.balance(), (await BinanceService.getTradingPair(wallet.token, config["CURRENCY"])).price, wallet.id);
   }
 
   @Delete("/:id")
@@ -49,9 +46,9 @@ export class WalletController {
   @Description("Delete one of your wallets.")
   @Returns(200, Wallet)
   async deleteWallet(@PathParams("id") id: string, @Req() req: Req): Promise<any> {
-    await this.walletService.delete({id: id});
+    await this.walletService.delete({ id: id });
     return true;
   }
 
-  constructor(private walletService: WalletService) {}
+  constructor(private walletService: WalletService) { }
 }
