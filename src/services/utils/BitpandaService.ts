89d9d5aa8a2abd71c42f57fc4b7d9a321a39c602
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 18:03:30 +0200

    Switched from decorators to jsdoc

--- src/services/utils/BitpandaService.ts
+++ src/services/utils/BitpandaService.ts
@@ -1,22 +1,34 @@
-import { Description } from "@tsed/schema";
 import axios from "axios";
 import { User } from "src/models/entity/User";
 import { SupportedTokens } from "src/models/SupportedTokens";
 
-@Description("The BitpandaService is a simple api-wrapper over the bitpanda api.")
+/**
+ * The BitpandaService is a simple api-wrapper over the bitpanda api.
+ */
 export class BitpandaService {
 
-    @Description("Returns the current prices of all crypto tokens listed on bitpanda")
+    /**
+     * Returns the current prices of all crypto tokens listed on bitpanda.
+     * @returns 
+     */
     public static async getPrices() {
         return (await axios.get("https://api.bitpanda.com/v1/ticker")).data;
     }
 
-    @Description("Returns the current prices of a specific crypto token listed on bitpanda")
+    /**
+     * Returns the current prices of a specific crypto token listed on bitpanda.
+     * @param token The name of the token you want the prices of.
+     * @returns 
+     */
     public static async getTokenPrices(token: SupportedTokens) {
         return (await this.getPrices())[token.toString()];
     }
 
-    @Description("Returns your bitpanda crypto wallets")
+    /**
+     * Returns your bitpanda crypto token wallets
+     * @param user The user who's api key (and therefore account) will be used.
+     * @returns 
+     */
     public static async getWallets(user: User) {
         return (
             await axios.get("https://api.bitpanda.com/v1/wallets", {
@@ -27,7 +39,11 @@ export class BitpandaService {
         ).data.data;
     }
 
-    @Description("Returns your bitpanda crypto index wallets")
+    /**
+     * Returns your bitpanda crypto index wallets
+     * @param user The user who's api key (and therefore account) will be used.
+     * @returns 
+     */
     public static async getIndices(user: User) {
         return (
             await axios.get("https://api.bitpanda.com/v1/asset-wallets", {
