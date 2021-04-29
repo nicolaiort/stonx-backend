aa08fc1b15b0391b532289bafeb602b7629dda91
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 18:10:05 +0200

    Added cunction to get single price

--- src/services/utils/BinanceService.ts
+++ src/services/utils/BinanceService.ts
@@ -1,3 +1,4 @@
+import { NotFound } from "@tsed/exceptions";
 import axios from "axios";
 import { BinanceTradingPair } from "src/models/BinanceTradingPair";
 
@@ -5,12 +6,22 @@ import { BinanceTradingPair } from "src/models/BinanceTradingPair";
  * The BinanceService is a simple api-wrapper over the binance api - mainly used to get pricing data.
  */
 export class BinanceService {
-    public static async getTradingPairs() {
+    public static async getTradingPairs(): Promise<BinanceTradingPair[]> {
         const res = (await axios.get("https://api.binance.com/api/v3/ticker/price")).data;
         let pairs = new Array<BinanceTradingPair>();
         for (let pair of res) {
-            pairs.push(pair.symbol, pair.price)
+            pairs.push(new BinanceTradingPair(pair.symbol, pair.price));
         }
         return pairs;
+    }
+    public static async getTradingPair(token: string, currency: string): Promise<BinanceTradingPair> {
+        const res = (await axios.get("https://api.binance.com/api/v3/ticker/price")).data;
+        let pairs = new Array<BinanceTradingPair>();
+        for (let pair of res) {
+            if (pair.symbol == `${token}${currency}`) {
+                return new BinanceTradingPair(pair.symbol, pair.price);
+            }
+        }
+        throw new NotFound(`Pair for the token ${token} and the currency/token ${currency} could not be found.`)
     }
 }
