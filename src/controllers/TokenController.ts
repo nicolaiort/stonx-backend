71e5cdada8c3afddc7f0956d59b89f8948a67620
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 16:03:57 +0200

    Removed bs imports

--- src/controllers/TokenController.ts
+++ src/controllers/TokenController.ts
@@ -1,13 +1,6 @@
 import { BodyParams, Controller, Get, Post, QueryParams, Req } from "@tsed/common";
-import { Authenticate, Authorize } from "@tsed/passport";
 import { Description, Returns } from "@tsed/schema";
-import axios from "axios";
-import { config } from "src/config/env";
-import { User } from "src/models/entity/User";
 import { SupportedTokens } from "src/models/SupportedTokens";
-import { Wallet } from "src/models/Wallet";
-import { WalletCreation } from "src/models/WalletCreation";
-import { WalletService } from "src/services/users/WalletService";
 
 @Controller("/tokens")
 export class TokenController {
