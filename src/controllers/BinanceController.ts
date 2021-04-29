c542c0e8c422a74f0a9d53392aae036803d319b9
Nicolai Ort <info@nicolai-ort.com>
2021-04-29 18:05:07 +0200

    Added binance controller

---
+++ src/controllers/BinanceController.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from "@tsed/common";
+import { Authenticate } from "@tsed/passport";
+import { Description, Returns, Security } from "@tsed/schema";
+import { BinanceTradingPair } from "src/models/BinanceTradingPair";
+import { BinanceService } from "src/services/utils/BinanceService";
+
+@Controller("/binance")
+export class BinanceController {
+    @Get("/prices")
+    @Authenticate("jwt")
+    @Security("jwt")
+    @Description("Returns the current crypto token prices listen on bitpanda")
+    @Returns(200)
+    async getPrices(): Promise<BinanceTradingPair[]> {
+        return await BinanceService.getTradingPairs();
+    }
+}
