a746dfba463b54efb8ce6fa8562542aab9480e8f
Nicolai Ort <info@nicolai-ort.com>
2021-05-01 09:28:07 +0200

    Added update function to user service

--- src/services/entity/UserService.ts
+++ src/services/entity/UserService.ts
@@ -1,3 +1,4 @@
+import { UserUpdating } from "src/models/UserUpdating";
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "../../models/entity/User";
 import { UserCreation } from "../../models/UserCreation";
@@ -31,6 +32,22 @@ export class UserService extends Repository<User> {
     user = await this.findOneOrFail(user);
     user.jwt_count = user.jwt_count + 1;
     await this.save(user);
+  }
+
+  async updateById(id: string, update_user: UserUpdating): Promise<User> {
+    const user = await this.findOneOrFail({ id: id });
+    user.email = update_user.email;
+    user.username = update_user.username;
+
+    if (update_user.bitpanda_api_key) {
+      user.bitpanda_api_key = update_user.bitpanda_api_key;
+    }
+
+    if (update_user.password) {
+      user.setPassword(update_user.password);
+    }
+
+    return await this.save(user);
   }
 
   async deleteById(id: string): Promise<User | undefined> {
