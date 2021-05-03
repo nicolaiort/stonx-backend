b0968852256fba42617d3f683bf724bf702e3e2a
Nicolai Ort <info@nicolai-ort.com>
2021-05-03 18:31:17 +0200

    Updated model

--- src/models/UserCreation.ts
+++ src/models/UserCreation.ts
@@ -1,10 +1,12 @@
-import { Description } from "@tsed/schema";
+import { Description, Optional, Required } from "@tsed/schema";
 import { Credentials } from "./Credentials";
 
 export class UserCreation extends Credentials {
   @Description("username")
+  @Required()
   username: string;
 
   @Description("Your bitpanda API key - scope Guthaben only!")
+  @Optional()
   bitpanda_api_key: string;
 }
