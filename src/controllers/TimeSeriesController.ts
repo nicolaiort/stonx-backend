3cfb12359ca090f721b03d80871b772f86a28f03
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 15:26:13 +0200

    Implemented getting bitpanda assets

--- src/controllers/TimeSeriesController.ts
+++ src/controllers/TimeSeriesController.ts
@@ -2,17 +2,19 @@ import { Controller, Get, PathParams, Req } from "@tsed/common";
 import { Authenticate } from "@tsed/passport";
 import { Description, Returns, Security } from "@tsed/schema";
 import { ExchangeAssetTimeSeries } from "src/models/entity/timeseries/ExchangeAssetTimeSeries";
+import { User } from "src/models/entity/User";
+import { TimeSeriesRanges } from "src/models/enums/TimeSeriesRanges";
 import { TimeSeriesService } from "src/services/entity/TimeSeriesService";
 
 @Controller("/timeseries")
 export class TimeSeriesController {
-    @Get("/bitpanda/wallets/:token")
+    @Get("/bitpanda/wallets/:token/:range")
     @Authenticate("jwt")
     @Security("jwt")
     @Description("TODO:")
     @Returns(200)
-    async getTradingPair(@PathParams("token") token: string, @Req() req: Req): Promise<ExchangeAssetTimeSeries[]> {
-        return [];
+    async getBitpandaWalletTimeSeries(@PathParams("token") token: string, @PathParams("range") range: TimeSeriesRanges, @Req() req: Req): Promise<ExchangeAssetTimeSeries[]> {
+        return this.timeSeriesService.findBitpandaWalletByUserAndRange((req.user as User), token, range);
     }
 
     constructor(private timeSeriesService: TimeSeriesService) { }
--- src/services/entity/TimeSeriesService.ts
+++ src/services/entity/TimeSeriesService.ts
@@ -1,6 +1,9 @@
 import { CryptoWalletTimeSeries } from "src/models/entity/timeseries/CryptoWalletTimeSeries";
 import { ExchangeAssetTimeSeries } from "src/models/entity/timeseries/ExchangeAssetTimeSeries";
-import { getConnectionManager, Repository } from "typeorm";
+import { User } from "src/models/entity/User";
+import { SupportedExchanges } from "src/models/enums/SupportedExchanges";
+import { TimeSeriesRanges } from "src/models/enums/TimeSeriesRanges";
+import { getConnectionManager, MoreThan, Repository } from "typeorm";
 
 export class TimeSeriesService {
 
@@ -18,5 +21,35 @@ export class TimeSeriesService {
 
   public async saveWalletDatapoint(datapoint: CryptoWalletTimeSeries) {
     return this.walletTimeSeriesService.save(datapoint);
+  }
+
+  public async findBitpandaWalletByUserAndRange(owner: User, asset: string, range: TimeSeriesRanges): Promise<ExchangeAssetTimeSeries[]> {
+    const now = new Date();
+    switch (range) {
+      case TimeSeriesRanges.ALL:
+        const all = await this.exchangeTimeSeriesService.find({ owner_id: owner.id, asset_name: asset, exchange: SupportedExchanges.BITPANDA });
+        return all.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.MONTH:
+        now.setMonth(now.getMonth() - 1);
+        const allInMonth = await this.exchangeTimeSeriesService.find({ owner_id: owner.id, asset_name: asset, exchange: SupportedExchanges.BITPANDA, timestamp: MoreThan(now.getTime()) })
+        return allInMonth.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.WEEK:
+        now.setMonth(now.setDate(now.getDate() - 7));
+        const allInWeek = await this.exchangeTimeSeriesService.find({ owner_id: owner.id, asset_name: asset, exchange: SupportedExchanges.BITPANDA, timestamp: MoreThan(now.getTime()) })
+        return allInWeek.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.DAY:
+        return this.exchangeTimeSeriesService.find({ owner_id: owner.id, asset_name: asset, exchange: SupportedExchanges.BITPANDA, timestamp: MoreThan(now.getTime() - 24 * 60 * 60 * 1000) });
+      default:
+        throw new Error("Not supported yet.");
+    }
   }
 }
