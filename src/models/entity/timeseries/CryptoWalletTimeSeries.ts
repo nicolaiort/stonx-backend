f6b26a7ca7ea51e8c160371e1b75ac04df6a9244
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 14:08:55 +0200

    Removed duplicate information that shouldn't change

--- src/models/entity/timeseries/CryptoWalletTimeSeries.ts
+++ src/models/entity/timeseries/CryptoWalletTimeSeries.ts
@@ -11,18 +11,14 @@ export class CryptoWalletTimeSeries extends TimeSeriesEntry {
   @Required()
   wallet_id: string;
 
-  @Column({ nullable: false })
-  @Required()
-  wallet_address: string;
 
   @Column({ nullable: false, type: "text" })
   @Required()
   token: SupportedTokens;
 
-  constructor(owner: User, timestamp: number, balance: number, fiat_value: number, id: string, address: string, token: SupportedTokens) {
+  constructor(owner: User, timestamp: number, balance: number, fiat_value: number, id: string, token: SupportedTokens) {
     super(owner, timestamp, balance, fiat_value);
     this.wallet_id = id;
-    this.wallet_address = address;
     this.token = token;
   }
 }
