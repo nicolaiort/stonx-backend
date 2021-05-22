43388106203d4a9806ecab07bd32aaddaf5635d1
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 13:32:46 +0200

    Implemented basis for saveing bitpanda timeseries data.

--- src/TimeSeriesManager.ts
+++ src/TimeSeriesManager.ts
@@ -1,25 +1,30 @@
 import cron from 'node-cron';
 import { CryptoWallet } from './models/entity/CryptoWallet';
+import { ExchangeAssetTimeSeries } from './models/entity/timeseries/ExchangeAssetTimeSeries';
 import { User } from './models/entity/User';
 import { SupportedExchanges } from './models/enums/SupportedExchanges';
+import { ExchangeService } from './services/entity/ExchangeService';
 import { TimeSeriesService } from './services/entity/TimeSeriesService';
 import { UserService } from './services/entity/UserService';
 import { WalletService } from './services/entity/WalletService';
+import { BitpandaService } from './services/utils/BitpandaService';
 
 export class TimeSeriesManager {
 
     private timeSeriesService: TimeSeriesService;
     private userService: UserService;
     private walletService: WalletService;
+    private exchangeService: ExchangeService;
 
     constructor() {
         this.timeSeriesService = new TimeSeriesService();
         this.userService = new UserService();
         this.walletService = new WalletService();
+        this.exchangeService = new ExchangeService();
     }
 
     public async init() {
-        this.scheduleQuaters()
+        // this.scheduleQuaters();
     }
 
     private scheduleQuaters() {
@@ -38,7 +43,7 @@ export class TimeSeriesManager {
 
     private async collectUserData(user: User, timestamp: number) {
         if (user.linkedExchanges.includes(SupportedExchanges.BITPANDA)) {
-            this.collectUserBitpandaData(timestamp);
+            this.collectUserBitpandaData(user, timestamp);
         }
         if (user.linkedExchanges.includes(SupportedExchanges.BINANCE)) {
             this.collectUserBinanceData(timestamp);
@@ -52,8 +57,18 @@ export class TimeSeriesManager {
         //TODO:
     }
 
-    private async collectUserBitpandaData(timestamp: number) {
-        //TODO:
+    private async collectUserBitpandaData(user: User, timestamp: number) {
+        const config = await this.exchangeService.findBitpandaByUserOrFail(user);
+        BitpandaService.getWallets(config).then((wallets) => {
+            for (const wallet of wallets) {
+                this.timeSeriesService.saveExchangeDatapoint(new ExchangeAssetTimeSeries(user, timestamp, wallet.balance, wallet.fiat, SupportedExchanges.BITPANDA, wallet.token));
+            }
+        });
+        BitpandaService.getIndices(config).then((indices) => {
+            for (const index of indices) {
+                this.timeSeriesService.saveExchangeDatapoint(new ExchangeAssetTimeSeries(user, timestamp, index.balance, index.balance, SupportedExchanges.BITPANDA, index.token));
+            }
+        });
     }
 
     private async collectUserBinanceData(timestamp: number) {
