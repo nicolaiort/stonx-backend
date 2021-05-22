edb005d75f07705cf377ebcc5a1671039e19d66e
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 11:51:28 +0200

    Removed wrong imports

--- src/TimeSeriesManager.ts
+++ src/TimeSeriesManager.ts
@@ -1,14 +1,6 @@
 import cron from 'node-cron';
-import { getConnectionManager, Repository } from "typeorm";
-import { TimeSeriesService } from "./services/entity/TimeSeriesService";
 
 export class TimeSeriesManager {
-
-    private timeSeriesService: Repository<TimeSeriesService>;
-
-    constructor() {
-        this.timeSeriesService = getConnectionManager().get().getRepository(TimeSeriesService);
-    }
 
     public async init() {
         cron.schedule('* * * * *', () => {
