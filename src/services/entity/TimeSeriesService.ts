92da69cf0aecfaf9a72d8d02b312ec04f7a8efe2
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 16:37:57 +0200

    Added TimeSeriesService functions to interact with the new entity

--- src/services/entity/TimeSeriesService.ts
+++ src/services/entity/TimeSeriesService.ts
@@ -1,5 +1,6 @@
 import { CryptoWalletTimeSeries } from "src/models/entity/timeseries/CryptoWalletTimeSeries";
 import { ExchangeAssetTimeSeries } from "src/models/entity/timeseries/ExchangeAssetTimeSeries";
+import { TotalPortfolioTimeSeries } from "src/models/entity/timeseries/TotalPortfolioTimeSeries";
 import { User } from "src/models/entity/User";
 import { SupportedExchanges } from "src/models/enums/SupportedExchanges";
 import { SupportedTokens } from "src/models/enums/SupportedTokens";
@@ -13,10 +14,12 @@ export class TimeSeriesService {
 
   private walletTimeSeriesService: Repository<CryptoWalletTimeSeries>;
   private exchangeTimeSeriesService: Repository<ExchangeAssetTimeSeries>;
+  private portfolioTimeSeriesService: Repository<TotalPortfolioTimeSeries>;
 
   constructor() {
     this.walletTimeSeriesService = getConnectionManager().get().getRepository(CryptoWalletTimeSeries);
     this.exchangeTimeSeriesService = getConnectionManager().get().getRepository(ExchangeAssetTimeSeries);
+    this.portfolioTimeSeriesService = getConnectionManager().get().getRepository(TotalPortfolioTimeSeries);
   }
 
   /**
@@ -35,6 +38,15 @@ export class TimeSeriesService {
    */
   public async saveWalletDatapoint(datapoint: CryptoWalletTimeSeries) {
     return this.walletTimeSeriesService.save(datapoint);
+  }
+
+  /**
+   * Create a new portfolio timeseries datapoint.
+   * @param datapoint The new datapoint.
+   * @returns The datapoint entity wrapped in a repository.save promise.
+   */
+  public async savePortfolioDatapoint(datapoint: TotalPortfolioTimeSeries) {
+    return this.portfolioTimeSeriesService.save(datapoint);
   }
 
   /**
@@ -165,6 +177,49 @@ export class TimeSeriesService {
         });
       case TimeSeriesRanges.TODAY:
         return this.walletTimeSeriesService.find({ owner_id: owner.id, token: token, wallet_id: id, timestamp: MoreThan(now.getTime() - 24 * 60 * 60 * 1000) });
+      default:
+        throw new Error("Not supported yet.");
+    }
+  }
+
+  /**
+   * Provides you with the timeseries datapoints for the whole portfolio in various ranges.
+   * @param owner The portfolios's owner.
+   * @param range The range in which you want your results to reside in.
+   * @returns An array of timeseries datapoints wrapped in a Promise.
+   */
+  public async findPortfolioByOwner(owner: User, range: TimeSeriesRanges): Promise<TotalPortfolioTimeSeries[]> {
+    const now = new Date();
+    switch (range) {
+      case TimeSeriesRanges.ALL:
+        const all = await this.portfolioTimeSeriesService.find({ owner_id: owner.id });
+        return all.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.THISYEAR:
+        now.setFullYear(now.getFullYear() - 1);
+        const allInYear = await this.portfolioTimeSeriesService.find({ owner_id: owner.id, timestamp: MoreThan(now.getTime()) })
+        return allInYear.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.THISMONTH:
+        now.setMonth(now.getMonth() - 1);
+        const allInMonth = await this.portfolioTimeSeriesService.find({ owner_id: owner.id, timestamp: MoreThan(now.getTime()) })
+        return allInMonth.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.THISWEEK:
+        now.setMonth(now.setDate(now.getDate() - 7));
+        const allInWeek = await this.portfolioTimeSeriesService.find({ owner_id: owner.id, timestamp: MoreThan(now.getTime()) })
+        return allInWeek.filter((d) => {
+          let date = new Date(d.timestamp);
+          return date.getHours() == 0 && date.getMinutes() == 0;
+        });
+      case TimeSeriesRanges.TODAY:
+        return this.portfolioTimeSeriesService.find({ owner_id: owner.id, timestamp: MoreThan(now.getTime() - 24 * 60 * 60 * 1000) });
       default:
         throw new Error("Not supported yet.");
     }
