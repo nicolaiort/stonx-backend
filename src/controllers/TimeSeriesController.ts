106e5ee6837ff04a4db350e0f0e32966f7fa1168
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 15:42:31 +0200

    Added description

--- src/controllers/TimeSeriesController.ts
+++ src/controllers/TimeSeriesController.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, PathParams, Req } from "@tsed/common";
 import { Authenticate } from "@tsed/passport";
 import { Description, Returns, Security } from "@tsed/schema";
+import { CryptoWalletTimeSeries } from "src/models/entity/timeseries/CryptoWalletTimeSeries";
 import { ExchangeAssetTimeSeries } from "src/models/entity/timeseries/ExchangeAssetTimeSeries";
 import { User } from "src/models/entity/User";
+import { SupportedTokens } from "src/models/enums/SupportedTokens";
 import { TimeSeriesRanges } from "src/models/enums/TimeSeriesRanges";
 import { TimeSeriesService } from "src/services/entity/TimeSeriesService";
 
@@ -33,6 +35,15 @@ export class TimeSeriesController {
     @Returns(200)
     async getBinanceSpotTimeSeries(@PathParams("token") token: string, @PathParams("range") range: TimeSeriesRanges, @Req() req: Req): Promise<ExchangeAssetTimeSeries[]> {
         return this.timeSeriesService.findBinanceSpotWalletsByUserAndRange((req.user as User), token, range);
+    }
+
+    @Get("/wallets/:token/:id/:range")
+    @Authenticate("jwt")
+    @Security("jwt")
+    @Description("Returns timeseries data for one of your custom wallets identified by it's id in the desired range.")
+    @Returns(200)
+    async getWalletTimeSeries(@PathParams("id") id: string, @PathParams("token") token: SupportedTokens, @PathParams("range") range: TimeSeriesRanges, @Req() req: Req): Promise<CryptoWalletTimeSeries[]> {
+        return this.timeSeriesService.findWalletByIdUserTokenAndRange((req.user as User), token, id, range);
     }
 
     constructor(private timeSeriesService: TimeSeriesService) { }
