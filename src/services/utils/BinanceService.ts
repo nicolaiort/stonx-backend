5609f32dac8d4fe34d0e28761d963aa0dd3998f0
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 14:22:01 +0200

    Added comments to binance controllers

--- src/services/utils/BinanceService.ts
+++ src/services/utils/BinanceService.ts
@@ -9,6 +9,11 @@ import { Wallet } from "../../models/Wallet";
  * The BinanceService is a simple api-wrapper over the binance api - mainly used to get pricing data.
  */
 export class BinanceService {
+
+    /**
+     * Get's all currently supported trading pairs from the Binance api alongside their current price.
+     * @returns Array of BinanceTradingPair objects wrapped into a promise.
+     */
     public static async getTradingPairs(): Promise<BinanceTradingPair[]> {
         const res = (await axios.get("https://api.binance.com/api/v3/ticker/price")).data;
         let pairs = new Array<BinanceTradingPair>();
@@ -18,11 +23,23 @@ export class BinanceService {
         return pairs;
     }
 
+    /**
+     * Get's a trading pair straight from the Binance api.
+     * @param token The source token of the trading pair.
+     * @param currency The target token/currency of the trading pair.
+     * @returns BinanceTradingPair object
+     */
     public static async getTradingPair(token: string, currency: string): Promise<BinanceTradingPair> {
         const pairs = await this.getTradingPairs();
         return this.getPairFromList(token, currency, pairs);
     }
 
+    /**
+     * Get's all of the user's Binance spot wallets and converts them to wallet objects.
+     * @param token The user's Binance api token/key.
+     * @param secret The user's Binance api secret.
+     * @returns Array of Wallet objects wrapped into a promise.
+     */
     public static async getSpotWallets(token: string, secret: string): Promise<Wallet[]> {
         const binance = new Binance().options({
             APIKEY: token,
@@ -54,6 +71,13 @@ export class BinanceService {
         return returnWallets;
     }
 
+    /**
+     * Filters a desired trading pair from an array of BinanceTradingPairs with some workaround for non-existing conversions.
+     * @param token The source token of the trading pair.
+     * @param currency The target token/currency of the trading pair.
+     * @param prices The array of BinanceTradingPairs
+     * @returns The desired BinanceTradingPair
+     */
     private static getPairFromList(token: string, currency: string, prices: Array<any>): BinanceTradingPair {
         let pair = prices.filter((p) => {
             return p.symbol == `${token}${currency}`;
