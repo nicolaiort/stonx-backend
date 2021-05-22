2ecd82733e7b61b142d256013a1d8065d75e6b91
Nicolai Ort <info@nicolai-ort.com>
2021-05-22 11:28:46 +0200

    Added barebones service for timeseries stuff

---
+++ src/services/entity/TimeSeriesService.ts
@@ -0,0 +1,14 @@
+import { CryptoWalletTimeSeries } from "src/models/entity/timeseries/CryptoWalletTimeSeries";
+import { ExchangeAssetTimeSeries } from "src/models/entity/timeseries/ExchangeAssetTimeSeries";
+import { getConnectionManager, Repository } from "typeorm";
+
+export class TimeSeriesService {
+
+  private walletTimeSeriesService: Repository<CryptoWalletTimeSeries>;
+  private exchangeTimeSeriesService: Repository<ExchangeAssetTimeSeries>;
+
+  constructor() {
+    this.walletTimeSeriesService = getConnectionManager().get().getRepository(CryptoWalletTimeSeries);
+    this.exchangeTimeSeriesService = getConnectionManager().get().getRepository(ExchangeAssetTimeSeries);
+  }
+}
