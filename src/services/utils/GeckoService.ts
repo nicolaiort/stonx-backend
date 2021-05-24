efa8137ec1fa0623cd796f2b23ee14960c998784
Nicolai Ort <info@nicolai-ort.com>
2021-05-24 15:24:53 +0200

    Added basic coingecko cacheing

--- src/services/utils/GeckoService.ts
+++ src/services/utils/GeckoService.ts
@@ -1,7 +1,14 @@
 import { NotFound } from "@tsed/exceptions";
 import axios from "axios";
+import NodeCache from 'node-cache';
 import { config } from "../../config/env";
 import { SupportedTokens } from "../../models/enums/SupportedTokens";
+
+/*
+ * This is the cache object that caches coingecko responses for 100 seconds before calling the api again.
+ * We do this to mitigate getting banned by the api for making too many requests.
+*/
+const geckoCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 /**
  * The GeckoService is a api-wrapper the CoinGecko api.
@@ -41,6 +48,10 @@ export class GeckoService {
      * @returns The token's id on coingecko.
      */
     public static async getTokenIdFromSymbol(token_symbol: string): Promise<string> {
+        const cached = geckoCache.get(`symbol-${token_symbol}`);
+        if (cached) {
+            return cached as string;
+        }
         const res_id = await axios.get(
             `https://api.coingecko.com/api/v3/coins/list`
         );
@@ -50,6 +61,7 @@ export class GeckoService {
         if (!token_id || token_id == '') {
             throw new NotFound("Token not found");
         }
+        geckoCache.set(`symbol-${token_symbol}`, token_id, 172800);
         return token_id;
     }
 
@@ -59,9 +71,14 @@ export class GeckoService {
      * @returns The token's price in the configured currency.
      */
     public static async getPrice(token_id: string): Promise<number> {
+        const cached = geckoCache.get(token_id);
+        if (cached) {
+            return cached as number;
+        }
         const res = await axios.get(
             `https://api.coingecko.com/api/v3/simple/price?ids=${token_id}&vs_currencies=${config["CURRENCY"].toString().toLowerCase()}`
         );
+        geckoCache.set(token_id, parseFloat(res.data[token_id][config["CURRENCY"].toString().toLowerCase()]));
         return parseFloat(res.data[token_id][config["CURRENCY"].toString().toLowerCase()]);
     }
 }
